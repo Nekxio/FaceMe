d4f7a374c7d4d8bf542b27956257368e17fab0a7
Edouard Bucamp <edouard.bucamp@gmail.com>
2020-11-17 13:20:53 +0100

    modification style page index

--- vues/accueil.php
+++ vues/accueil.php
@@ -1,3 +1,27 @@
+    <div id="headerFond">
+        <div class="container headerFlex">
+            <div id="headerLogo">
+                <img src="./icons/logo.svg" alt="Logo FaceMe">
+            </div>
+            <div id="headerSearch">
+                <img src="./icons/loupe.svg" alt="Icone loupe">
+                <input type="text" placeholder="Rechercher" id="headerSearch__input">
+            </div>
+            <div id="headerUser">
+                <button id="headerUser__button" onclick="userHover()">
+                    <img src="./icons/user_white.svg" alt="Icone utilisateur">
+                </button>
+                <div id="headerHover__menu">
+                    <ul>
+                        <li><a href="">Mon Profil</a></li>
+                        <li><a href="">Mes Paramètres</a></li>
+                        <li><a href="">Se déconnecter</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php
  if(!isset($_SESSION["id"])) {
         // On n est pas connecté, il faut retourner à la pgae de login
@@ -39,33 +63,59 @@
         $query -> execute(array($_SESSION["id"]));
         while($result = $query -> fetch()){
     ?>
-    
+
+    <section class="container">
+
     <div class="post_complet">
-        <div class="post_user">
-            <h1><?= $result['name'] ?></h1>
-            <p><?= $result['contenu'] ?></p>
-            <img src="<?= $result['contenu'] ?>" alt="image de <?= $result['name'] ?>."/>
-        </div>
-        <div>
-            <h1>Commentaires</h1>
-            <form action='index.php?action=commentaires&idPost=<?= $result['id']?>' method='POST'>
-                <label><b><?= $result['name'] ?></b></label>
-                <input type='text' placeholder='Ecrire une réponse...' name='contenu' required>
-                <input type='submit' id='submit' value='Envoyer' >
-            </form>
-        </div>
-        <div>
-            <?php
-                $sql1 = "SELECT commentaires.*, user.name FROM commentaires JOIN user ON commentaires.idAuteur=user.id WHERE idPost=? order by dateCom DESC";
-                $query1 = $pdo -> prepare($sql1);
-                $query1 -> execute(array($result["id"]));
-                while($result1 = $query1 -> fetch()){
-            ?>
-                <h1><?= $result1['name'] ?></h1>
-                <p><?= $result1['contenu'] ?></p>
-                <?php } ?>
+        <div class="post_completpadding">
+            <div>
+                <div class="post_user">
+                    <img src="./icons/user_orange.svg" alt="icône user orange">
+                    <h1><?= $result['name'] ?></h1>
+                </div>
+                <div class="post_contenu">
+                    <p><?= $result['contenu'] ?></p>
+                    <?php
+                        if(isset($result['image'])){
+                    ?>
+                        <img src="<?= $result['image'] ?>" alt="image de <?= $result['name'] ?>."/>
+                    <?php
+                    }
+                ?>
+                </div>
+            </div>
+            <hr class="separation_grise">
+            <div class="post_commentaires">
+                <h1>Commentaires</h1>
+                <form action='index.php?action=commentaires&idPost=<?= $result['id']?>' method='POST' class="post_commentairesForm">
+                    <label><?= $result['name'] ?></label>
+                    <input type='text' placeholder='Écrire un commentaire' name='contenu' required class="post_commentairesInput">
+                    <hr class="separation_orange">
+                    <div class="post_commentairesFlex">
+                        <button type='file' class="post_commentairesIcon">
+                            <img src="./icons/add.svg" alt="icône ajout fichier">
+                        </button>
+                        <button type='submit' class="post_commentairesIcon">
+                            <img src="./icons/send.svg" alt="icône envoyer">
+                        </button>
+                    </div>  
+                </form>
+            </div>
+            <div>
+                <?php
+                    $sql1 = "SELECT commentaires.*, user.name FROM commentaires JOIN user ON commentaires.idAuteur=user.id WHERE idPost=? order by dateCom DESC";
+                    $query1 = $pdo -> prepare($sql1);
+                    $query1 -> execute(array($result["id"]));
+                    while($result1 = $query1 -> fetch()){
+                ?>
+                    <h1><?= $result1['name'] ?></h1>
+                    <p><?= $result1['contenu'] ?></p>
+                    <?php } ?>
+            </div>
         </div>
     </div>
+
+    </section>
 <?php
         }
     }
