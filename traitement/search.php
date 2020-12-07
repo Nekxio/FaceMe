4d9a5adc07d40c5f997bb5bf136e095d507240ab
Nekxio <leohilaire.contact@gmail.com>
2020-12-07 10:35:23 +0100

    Modif page accueil

---
+++ traitement/search.php
@@ -0,0 +1,15 @@
+<?php
+include("../config/config.php");
+include("../config/bd.php");
+session_start();
+
+$keyword = $_GET['recherche'];
+$sql1 = "SELECT id,email,name FROM user WHERE name like '%$keyword%'";
+$query1 = $pdo->prepare($sql1);
+$query1->execute();
+while($result1 = $query1 -> fetch()){
+?>
+    <li><a href="index.php?action=profile&id=<?=$result1['id']?>"><?=$result1['name']?></a><a href="index.php?action=friendship&id=<?=$result1['id']?>"></a></li>
+<?php
+};
+?>
