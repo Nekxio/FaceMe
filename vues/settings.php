2e4799d2ecc7c40b470d5d9559af5f5338582607
Edouard Bucamp <edouard.bucamp@gmail.com>
2020-12-12 20:35:02 +0100

    Responsive page settings

--- vues/settings.php
+++ vues/settings.php
@@ -5,37 +5,46 @@ $q->execute(array($_SESSION['id']));
 while($result = $q -> fetch()){;
 ?>
 
-<div id="formIn__fond" >
-    <p id="formIn__title">Je modifie mes données :</p>
-    <div id="signUp__fond">
-        <form action="index.php?action=form_settings" method="POST" id="signUp__main" enctype="multipart/form-data">
-            <input type="text" placeholder="Nom complet" class="signUp__input" id="formName" name="name" value="<?=$result['name']?>" required>
-            <input type="email" placeholder="E-mail" class="signUp__input" id="formMail" name="email" value="<?=$result['email']?>" required>
-            <input type="text" placeholder="biographie" class="signUp__input" id="formBio" name="bio" value="<?=$result['bio']?>" required>
-            <input type="password" placeholder="Mot de passe" class="signUp__input" id="formPwd1" minlength='8' pattern='(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$' name="PASSWORD" required>
-            <input type="password" placeholder="Confirmer votre mot de passe" class="signUp__input" id="formPwd2" minlength='8' pattern='(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$' name="PASSWORD2" required>
-            <input type="submit" value="Mettre à jour !" class="form__submit" id="signUp__submit">
-        </form>
+<section class="container">
+    <div>
+        <p class="settingsTitle">Je modifie mes données :</p>
+        <div class="settingsCard">
+            <form action="index.php?action=form_settings" method="POST" enctype="multipart/form-data">
+                <div class="settingsCard__flex">
+                    <input type="text" placeholder="Nom complet" class="settings__input" id="formName" name="name" value="<?=$result['name']?>" required>
+                    <input type="email" placeholder="E-mail" class="settings__input" id="formMail" name="email" value="<?=$result['email']?>" required>
+                </div>
+                <div class="settingsCard__flex">
+                    <input type="password" placeholder="Mot de passe" class="settings__input" id="formPwd1" minlength='8' pattern='(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$' name="PASSWORD" required>
+                    <input type="password" placeholder="Confirmer votre mot de passe" class="settings__input" id="formPwd2" minlength='8' pattern='(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$' name="PASSWORD2" required>
+                </div>
+                <div class="settingsCard__flex">
+                    <input type="text" placeholder="biographie" class="settings__input" id="formBio" name="bio" value="<?=$result['bio']?>" required>
+                    <input type="submit" value="Mettre à jour" class="settings__submit">
+                </div>
+            </form>
+        </div>
     </div>
-</div>
-<div id="formIn__fond" >
-    <p id="formIn__title">Je modifie mon image de profil :</p>
-    <div id="signUp__fond">
-        <form action="index.php?action=form_avatar" method="POST" id="signUp__main" enctype="multipart/form-data">
-            <input type="file" name="avatar">
-            <input type="submit" value="Mettre à jour !" class="form__submit" id="signUp__submit">
-        </form>
+    <div>
+        <p class="settingsTitle">Je modifie mon image de profil :</p>
+        <div class="settingsCard">
+            <form action="index.php?action=form_avatar" method="POST" enctype="multipart/form-data" class="settingsCard__flexSubmit">
+                <input type="file" name="avatar" class="settings__inputsFile">
+                <input type="submit" value="Mettre à jour" class="settings__submit settings__submitSpecial">
+            </form>
+        </div>
     </div>
-</div>
-<div id="formIn__fond" >
-    <p id="formIn__title">Je modifie ma bannière :</p>
-    <div id="signUp__fond">
-        <form action="index.php?action=form_background" method="POST" id="signUp__main" enctype="multipart/form-data">
-            <input type="file" name="background">
-            <input type="submit" value="Mettre à jour !" class="form__submit" id="signUp__submit">
-        </form>
+    <div>
+        <p class="settingsTitle">Je modifie ma bannière :</p>
+        <div class="settingsCard">
+            <form action="index.php?action=form_background" method="POST" enctype="multipart/form-data" class="settingsCard__flexSubmit">
+                <input type="file" name="background" class="settings__inputsFile">
+                <input type="submit" value="Mettre à jour" class="settings__submit settings__submitSpecial">
+            </form>
+        </div>
     </div>
-</div>
+</section>
+
 <?php
 }
 ?>
